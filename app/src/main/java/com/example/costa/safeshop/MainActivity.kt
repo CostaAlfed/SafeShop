33aaba6c6dd08241ad9ab4907f478946763d8baa
Costa <alfedcosta@gmail.com>
2020-08-11 23:45:58 +0100

    11.08.2020 11:45 PM

--- app/src/main/java/com/example/costa/safeshop/MainActivity.kt
+++ app/src/main/java/com/example/costa/safeshop/MainActivity.kt
@@ -14,29 +14,34 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_reg.*
-import kotlinx.android.synthetic.main.activity_total.*
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import java.util.Base64.getEncoder
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import io.card.payment.i18n.StringKey
+import java.security.NoSuchAlgorithmException
+import java.security.spec.InvalidKeySpecException
 import java.util.*
-import javax.crypto.spec.SecretKeySpec
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 
 
 class MainActivity : AppCompatActivity() {
+    @Throws(NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+    fun getEncryptedPassword(password: String, salt: ByteArray): ByteArray {
+        val spec = PBEKeySpec(password.toCharArray(), salt, 20000, 160)
+        val f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+        return f.generateSecret(spec).getEncoded()
+    }
+
+    @Throws(NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+    fun authenticate(attemptedPassword: String, encryptedPassword: ByteArray, salt: ByteArray): Boolean {
+        // Encrypt the clear-text password using the same salt that was used to encrypt the original password
+        val encryptedAttemptedPassword = getEncryptedPassword(attemptedPassword, salt)
+
+        // Authentication succeeds if encrypted password that the user entered is equal to the stored hash
+        return Arrays.equals(encryptedPassword, encryptedAttemptedPassword)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        /*
+/*
         var logpass = login_password.text.toString()
         //System.out.println("--logpass= "+logpass)
         val plaintext: ByteArray = logpass.toByteArray()
@@ -49,28 +54,62 @@ class MainActivity : AppCompatActivity() {
         //System.out.println("--bytekey= "+bytekey)
         val StringKey = String(bytekey, Charsets.UTF_8)
         //System.out.println("--StringKey= "+StringKey)
-        println("--StringKey_typecheck"+"${StringKey::class.simpleName}"+"___expected: String")*/
+        println("--StringKey_typecheck"+"${StringKey::class.simpleName}"+"___expected: String")
+*/
+        val ipad: String = getString(R.string.local_ip)
+        val url3 ="http://" + ipad + "/SalesWeb/get_salt.php?number=" + login_number.text.toString()
+        val urlh ="http://" + ipad + "/SalesWeb/get_pwhash.php?number=" + login_number.text.toString()
+        var waiter1=0
+        var waiter2=0
 
-    signin_butt.setOnClickListener {
-        var StringKeyEq:String=""
-        var ipad:String=getString(R.string.local_ip)
-        var urlh =
-            "http://"+ipad+"/SalesWeb/get_strkey.php?number=" + login_number.text.toString()
-        var rqh: RequestQueue = Volley.newRequestQueue(this)
-        var srh= StringRequest(Request.Method.GET,urlh, Response.Listener { response ->
-            StringKeyEq=response
-        }, Response.ErrorListener { error ->
-            Toast.makeText(this,error.message, Toast.LENGTH_LONG).show()
-        })
-        rqh.add(srh)
+        var pwhashstr: String = ""
+        signin_butt.setOnClickListener {
+            var saltstr2: String ="initial string of salt."
+            val rq3: RequestQueue = Volley.newRequestQueue(this)
+            System.out.println("--bang2")
+            val sr3 = StringRequest(Request.Method.GET, url3, Response.Listener { response ->
+                var saltstr = response
+                saltstr2=saltstr
+                System.out.println(" --saltstr2= " + saltstr2)
+                waiter1++
+            }, Response.ErrorListener { error ->
+                Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
+            })
+            rq3.add(sr3)
 
+            val rqh: RequestQueue = Volley.newRequestQueue(this)
+            val srh = StringRequest(Request.Method.GET, urlh, Response.Listener { response ->
+                pwhashstr = response.toString()
+                System.out.println(" --pwhashstr= " + pwhashstr)
+                waiter2++
+            }, Response.ErrorListener { error ->
+                Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
+            })
+            rqh.add(srh)
+            var waittimes=0
+            while (waiter1==0 && waiter2==0) {
+                System.out.println("--waiter1= "+waiter1)
+                System.out.println("--waiter2= "+waiter2)
+                System.out.println("--wait times= "+waittimes)
+                Thread.sleep(3000)
+                System.out.println("")
+            }
+
+            //converting pwhashstr and saltstr2 back to BYTE ARRAY
+            val pwhash = pwhashstr.toByteArray(Charsets.UTF_8)
+            System.out.println(" --pwhash= " + Arrays.toString(pwhash))
+            val salt = saltstr2.toByteArray(Charsets.UTF_8)
+            System.out.println(" --salt= " + Arrays.toString(salt))
+
+/*
         val ByteKeyEq = Base64.getDecoder().decode(StringKeyEq)
         System.out.println("--byteKeyEq= "+ByteKeyEq)
         val originalKey = SecretKeySpec(ByteKeyEq, 0, ByteKeyEq.size, "AES")
         System.out.println("--originalKey= "+originalKey)
+*/
 
 
-        //--------------------------ENCRYPTION-----------------------------
+            /*/--------------------------ENCRYPTION-----------------------------
         val plaintext: ByteArray = login_password.text.toString().toByteArray()
         val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
         cipher.init(Cipher.ENCRYPT_MODE, originalKey)
@@ -81,84 +120,99 @@ class MainActivity : AppCompatActivity() {
         val logpassencripted = String(ciphertext, Charsets.UTF_8)
         System.out.println("--logpassencripted= "+logpassencripted)
 
-        //-----------------------------------------------------------------
-        //var url =
-        //            "http://"+ipad+"/SalesWeb/login.php?number=" + login_number.text.toString() + "&password=" +
-        //                    login_password.text.toString()
+        //-----------------------------------------------------------------*/
+            //var url =
+            //            "http://"+ipad+"/SalesWeb/login.php?number=" + login_number.text.toString() + "&password=" +
+            //                    login_password.text.toString()
+
+            if (authenticate(login_password.text.toString(), pwhash, salt).equals(false)) {
+                System.out.println(" --authenticate= false")
+/*
         var url =
-            "http://"+ipad+"/SalesWeb/login.php?number=" + login_number.text.toString() + "&password=" + logpassencripted
+            "http://"+ipad+"/SalesWeb/login.php?number=" + login_number.text.toString() + "&pwhash=" + encpass
         var rq: RequestQueue = Volley.newRequestQueue(this)
         var sr= StringRequest(Request.Method.GET,url, Response.Listener { response ->
             if (response.equals("0"))
-                Toast.makeText(this,"Sign In Failed", Toast.LENGTH_LONG).show()
-            else {
-                //var url2 =
-                  //  "http://"+ipad+"/SalesWeb/get_user_name.php?number=" + login_number.text.toString()
-                UserInfo.mobile=login_number.text.toString()
-                var i0 = Intent(this, HomeAct::class.java)
-                var uname:String=""               //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSSS DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS
-                if (login_number.text.toString().equals("0799123456")) uname="Anis" //DELETE THISSSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
-                if (login_number.text.toString().equals("0559235411")) uname="Smecta" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSS  DELETE THISSSSSS
-                if (login_number.text.toString().equals("0799789123")) uname="Costa" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
-                if (login_number.text.toString().equals("0669420420")) uname="Jon Doe" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
-                if (login_number.text.toString().equals("0555123456")) uname="Dwight" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
-                if (login_number.text.toString().equals("0700000000")) uname="Mouh" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
 
-                Toast.makeText(this,"Welcome, "+uname+"!", Toast.LENGTH_LONG).show()      //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS
+ */
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+            } else {
+                System.out.println(" --authenticate= true")
+                UserInfo.mobile = login_number.text.toString()
+                val i0 = Intent(this, HomeAct::class.java)
+                var uname: String =
+                    ""               //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSSS DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS
+                if (login_number.text.toString().equals("0799123456")) uname =
+                    "Anis" //DELETE THISSSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
+                if (login_number.text.toString().equals("0559235411")) uname =
+                    "Smecta" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSS  DELETE THISSSSSS
+                if (login_number.text.toString().equals("0799789123")) uname =
+                    "Costa" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
+                if (login_number.text.toString().equals("0669420420")) uname =
+                    "Jon Doe" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
+                if (login_number.text.toString().equals("0555123456")) uname =
+                    "Dwight" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
+                if (login_number.text.toString().equals("0700000000")) uname =
+                    "Mouh" //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS  DELETE THISSSSSS
+
+                Toast.makeText(this, "Welcome, " + uname + "!", Toast.LENGTH_LONG)
+                    .show()      //DELETE THISSSSSS DELETE THISSSSSS DELETE THISSSSSS
                 startActivity(i0)
 
             }
-        }, Response.ErrorListener { error ->
+/*        }, Response.ErrorListener { error ->
             Toast.makeText(this,error.message, Toast.LENGTH_LONG).show()
         })
         rq.add(sr)
     }
+*/
+        }
+            signup_butt.setOnClickListener {
+                val i = Intent(this, RegAct::class.java)
+                //intent.putExtra("secretkey",StringKey)
+                //System.out.println("--AES key (StringKey sent in mainact to regact) = "+StringKey)
+                startActivity(i)
+            }
 
-        signup_butt.setOnClickListener{
-            var i=Intent(this,RegAct::class.java)
-            //intent.putExtra("secretkey",StringKey)
-            //System.out.println("--AES key (StringKey sent in mainact to regact) = "+StringKey)
-            startActivity(i)
-        }
-
-        contact_us_butt.setOnClickListener {
-            if (contact_layout.isVisible)   contact_layout.visibility= View.INVISIBLE
-            else contact_layout.visibility= View.VISIBLE
-        }
-        twitter_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://twitter.com/costaalfed/")
-            startActivity(openURL)
-        }
-        facebook_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://www.facebook.com/costalfed/")
-            startActivity(openURL)
-        }
-        instagram_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://www.instagram.com/costa_alfed/")
-            startActivity(openURL)
-        }
-        reddit_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://www.reddit.com/user/Costaalfed")
-            startActivity(openURL)
-        }
-        linkdin_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://www.linkedin.com/in/costa-alfed-a18047176/")
-            startActivity(openURL)
-        }
-        whatsapp_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://wa.me/213791192226")
-            startActivity(openURL)
-        }
-        email_butt.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("mailto:alfedcosta@gmail.com")
-            startActivity(openURL)
+            contact_us_butt.setOnClickListener {
+                if (contact_layout.isVisible) contact_layout.visibility = View.INVISIBLE
+                else contact_layout.visibility = View.VISIBLE
+            }
+            twitter_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://twitter.com/costaalfed/")
+                startActivity(openURL)
+            }
+            facebook_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://www.facebook.com/costalfed/")
+                startActivity(openURL)
+            }
+            instagram_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://www.instagram.com/costa_alfed/")
+                startActivity(openURL)
+            }
+            reddit_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://www.reddit.com/user/Costaalfed")
+                startActivity(openURL)
+            }
+            linkdin_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://www.linkedin.com/in/costa-alfed-a18047176/")
+                startActivity(openURL)
+            }
+            whatsapp_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("https://wa.me/213791192226")
+                startActivity(openURL)
+            }
+            email_butt.setOnClickListener {
+                val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("mailto:alfedcosta@gmail.com")
+                startActivity(openURL)
+            }
         }
     }
-}
+
--- app/src/main/java/com/example/costa/safeshop/RegAct.kt
+++ app/src/main/java/com/example/costa/safeshop/RegAct.kt
@@ -21,14 +21,56 @@ import java.util.*
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
-import java.util.Base64.getEncoder
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+
 
 
 class RegAct : AppCompatActivity() {
 
+
+
+        @Throws(NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+        fun getEncryptedPassword(password: String, salt: ByteArray): ByteArray {
+
+            val derivedKeyLength = 160 // SHA-1 generates 160 bit hashes, so that's what makes sense here
+
+            val iterations = 20000 // Pick an iteration count that works for you.
+            //The NIST recommends at least 1,000 iterations: http://csrc.nist.gov/publications/nistpubs/800-132/nist-sp800-132.pdf
+
+            val spec = PBEKeySpec(password.toCharArray(), salt, iterations, derivedKeyLength)
+
+            // PBKDF2 with SHA-1 as the hashing algorithm. Note that the NIST
+            // specifically names SHA-1 as an acceptable hashing algorithm for PBKDF2
+            val f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+
+            return f.generateSecret(spec).getEncoded()
+        }
+
+        @Throws(NoSuchAlgorithmException::class)
+        fun generateSalt(): ByteArray {
+            // VERY important to use SecureRandom instead of just Random
+            val random = SecureRandom.getInstance("SHA1PRNG")
+
+            // Generate a 8 byte (64 bit) salt as recommended by RSA PKCS5
+            val salt = ByteArray(8)
+            random.nextBytes(salt)
+
+            return salt
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
+
+        /*
 
         val keygen = KeyGenerator.getInstance("AES")
         keygen.init(256)
@@ -49,38 +91,51 @@ class RegAct : AppCompatActivity() {
         //val byteKey2 = getDecoder().decode(StringKey2)
         //System.out.println("--byteKey2= "+byteKey2)
         // rebuild key using SecretKeySpec
+         */
+
         var tempcounter=0
 
         reg_butt.setOnClickListener{
             tempcounter++
+
+            var saltbae=generateSalt()
+            System.out.println(""+tempcounter+" --saltbae= "+Arrays.toString(saltbae))
+            var encpass=getEncryptedPassword(reg_password.text.toString(),saltbae)
+            System.out.println(""+tempcounter+" --encpass= "+Arrays.toString(encpass))
+            var encpassstr=Arrays.toString(encpass)
+            System.out.println(""+tempcounter+" --encpassstr= "+encpassstr)
+
+/*
+
             /*val signpass= reg_password.text.toString()
             System.out.println("--signpass= "+signpass)*/
             val bytesignpass: ByteArray = reg_password.text.toString().toByteArray(Charsets.UTF_8)
-            System.out.println(""+tempcounter + "--bytesignpass= "+Arrays.toString(bytesignpass))
+            //System.out.println(""+tempcounter + "--bytesignpass= "+Arrays.toString(bytesignpass))
             val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
             cipher.init(Cipher.ENCRYPT_MODE, key)
             val ciphertext: ByteArray = cipher.doFinal(bytesignpass)
-            System.out.println(""+tempcounter+"--ciphertext= "+Arrays.toString(ciphertext))
+            //System.out.println(""+tempcounter+"--ciphertext= "+Arrays.toString(ciphertext))
             val iv: ByteArray = cipher.iv
-            System.out.println(""+tempcounter+"--iv= "+Arrays.toString(iv))
+            //System.out.println(""+tempcounter+"--iv= "+Arrays.toString(iv))
             val signpassencripted = Arrays.toString(ciphertext)
-            System.out.println(""+tempcounter+"--signpassencripted= "+signpassencripted)
+            //System.out.println(""+tempcounter+"--signpassencripted= "+signpassencripted)
+ */
 
             if (reg_password.text.toString().equals(password_confirm.text.toString())) {
-                var ipad:String=getString(R.string.local_ip)
-                var url =
-                    "http://"+ipad+"/SalesWeb/add_user.php?number=" + reg_number.text.toString() + "&password=" +
-                            signpassencripted +"strkey="+StringKey2+ "&name=" + reg_name.text.toString() +
+                val ipad:String=getString(R.string.local_ip)
+                val url =
+                    "http://"+ipad+"/SalesWeb/add_user.php?number=" + reg_number.text.toString() + "&pwhash=" +
+                            encpassstr +"&salt="+Arrays.toString(saltbae)+ "&name=" + reg_name.text.toString() +
                             "&address=" + reg_address.text.toString()
-                var rq:RequestQueue=Volley.newRequestQueue(this)
-                var sr=StringRequest(Request.Method.GET,url,Response.Listener { response ->
+                val rq:RequestQueue=Volley.newRequestQueue(this)
+                val sr=StringRequest(Request.Method.GET,url,Response.Listener { response ->
                     if (response.equals("0"))
                     {
                         Toast.makeText(this,"Number already used!",Toast.LENGTH_LONG).show()
                         System.out.println(""+tempcounter+"--response= 0")} //DELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETE
                     else {System.out.println(""+tempcounter+"--response= 1") //DELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETEDELETE
                         UserInfo.mobile=reg_number.text.toString()
-                        var i = Intent(this, MainActivity::class.java)
+                        val i = Intent(this, MainActivity::class.java)
                         startActivity(i)
                     }
                 },Response.ErrorListener { error ->
@@ -89,9 +144,10 @@ class RegAct : AppCompatActivity() {
                 })
                 rq.add(sr)
             }
-            else
-                Toast.makeText(this,"Passwords don't match!",Toast.LENGTH_LONG).show()
-
+            else {
+                System.out.println("Passwords don't match!")
+                Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
